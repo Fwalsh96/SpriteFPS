90df6304cd6b55ce8ede16dea6759aab6093ee84
kehlert72 <kaleb.ehlert@gmail.com>
2020-08-07 19:44:32 -0400

    Fixed camera rotation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using SpriteFPS.General;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private float mouseSensivity;
+
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start() {
+        mouseSensivity = 100f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensivity, 0, 0);
+
+        player.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensivity, 0);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ namespace SpriteFPS.General {
     public class Player : MonoBehaviour {
         public float speed;
 
+        int floorMask;
+        float camRayLength = 100f;
+
         private float moveX;
         private float moveZ;
 
@@ -16,6 +19,10 @@ namespace SpriteFPS.General {
 
             moveX = 0;
             moveZ = 0;
+
+            floorMask = LayerMask.GetMask("Floor");
+
+            camRayLength = 100f;
 
             Cursor.lockState = CursorLockMode.Locked;
         }
@@ -46,26 +53,54 @@ namespace SpriteFPS.General {
         void FixedUpdate() {
             Move();
 
-            Turn();
+            //Turn();
         }
 
         // The function that handles the calls to move the player using vector math
         private void Move() {
-            // Use the two store floats to create a new Vector3 variable movement.
-            Vector3 movement = new Vector3(moveX, 0f, moveZ);
+            // Moves the player based on their direction
+            Vector3 movement = transform.right * moveX + transform.forward * moveZ;
 
             movement = movement.normalized * speed * Time.deltaTime;
 
             playerRigidbody.MovePosition(playerRigidbody.position + movement);
+
+            // Use the two store floats to create a new Vector3 variable movement.
+            //Vector3 movement = new Vector3(moveX, 0f, moveZ);
+
+            //movement = movement.normalized * speed * Time.deltaTime;
+
+            //playerRigidbody.MovePosition(playerRigidbody.position + movement);
         }
 
         // Moves the player around the world
         private void Turn() {
-            float rotateX = 5 * -Input.GetAxis("Mouse Y");
+            float rotateX = 250 * -Input.GetAxis("Mouse Y") * Time.deltaTime;
 
-            float rotateY = 5 * Input.GetAxis("Mouse X");
-            
-            Camera.main.transform.Rotate(rotateX, rotateY, 0);
+            this.transform.Rotate(rotateX, 0, 0);
         }
+
+        //private void Turn() {
+        //    // Create a ray from the mouse cursor on screen in the direction of the camera.
+        //    Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    // Create a RaycastHit variable to store information about what was hit by the ray.
+        //    RaycastHit floorHit;
+
+        //    // Perform the raycast and if it hits something on the floor layer...
+        //    if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+        //        // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+        //        Vector3 playerToMouse = floorHit.point - transform.position;
+
+        //        // Ensure the vector is entirely along the floor plane.
+        //        playerToMouse.y = 0f;
+
+        //        // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+        //        Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+        //        // Set the player's rotation to this new rotation.
+        //        playerRigidbody.MoveRotation(newRotation);
+        //    }
+        //}
     }
 }
