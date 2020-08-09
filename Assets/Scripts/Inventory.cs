b1c3245031ada4693141100bd21bf174dea2b1d1
kehlert72 <kaleb.ehlert@gmail.com>
2020-08-08 20:17:39 -0400

    Player can sprint

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO Create an item pickup
+
+public class Inventory : MonoBehaviour {
+    public static Inventory instance;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    private void Awake() {
+        if (instance != null) {
+            Debug.LogWarning("More than one instance of Inventory found!");
+
+            return;
+        }
+
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
