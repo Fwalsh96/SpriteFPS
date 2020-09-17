90391107d7346e80ef6573dcab32e3614b6b899c
ForrestW <fwalsh@bgsu.edu>
2020-09-17 00:15:14 -0400

    Created the basic main menu, linked it to the demo room

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Main Menu Button
+    // Load Game Button
+    // Options Button
+    // Exit to Desktop
+    // Demo Room Button
+    public void ActivateDemoRoom() {
+
+        SceneManager.LoadScene("Moving Room Objects");
+    }
+}
