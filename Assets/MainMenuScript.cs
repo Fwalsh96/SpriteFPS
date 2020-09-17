b20272c59fb9f0199b3d021826879e93ba498276
ForrestW <fwalsh@bgsu.edu>
2020-09-17 00:46:05 -0400

    Added options menu to the main menu

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -2,16 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Main Menu Button
+    public GameObject mainMenuPanel;
+
+    // New Game Button
+    public void NewGameButton() {
+        Debug.Log("Main Menu Button Pressed");
+    }
+
     // Load Game Button
+    public void LoadGameButton() {
+        Debug.Log("Load Game Button Pressed");
+    }
+
     // Options Button
+    public void OptionsButton() {
+        mainMenuPanel.SetActive(false);
+    }
+
+    // THIS BUTTON IS NOT WORKING
     // Exit to Desktop
+    public void ExitGameButton() {
+        // Consider adding are you sure
+        Application.Quit();
+    }
+
     // Demo Room Button
     public void ActivateDemoRoom() {
 
         SceneManager.LoadScene("Moving Room Objects");
+    }
+
+    // Back Button on options screen
+    public void BackButton() {
+        mainMenuPanel.SetActive(true);
     }
 }
